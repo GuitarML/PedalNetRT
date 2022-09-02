bdb0c67a7df831e93d154fed8888160010afb8da
Keith Bloemer <32459398+GuitarML@users.noreply.github.com>
2022-09-02 09:59:54 -0500

    Update train.py

--- train.py
+++ train.py
@@ -36,16 +36,17 @@ if __name__ == "__main__":
     parser.add_argument("in_file", nargs="?", default="data/in.wav")
     parser.add_argument("out_file", nargs="?", default="data/out.wav")
     parser.add_argument("--sample_time", type=float, default=100e-3)
+    parser.add_argument("--normalize", type=bool, default=True)
 
-    parser.add_argument("--num_channels", type=int, default=12)
-    parser.add_argument("--dilation_depth", type=int, default=10)
-    parser.add_argument("--num_repeat", type=int, default=1)
+    parser.add_argument("--num_channels", type=int, default=4)
+    parser.add_argument("--dilation_depth", type=int, default=9)
+    parser.add_argument("--num_repeat", type=int, default=2)
     parser.add_argument("--kernel_size", type=int, default=3)
 
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--learning_rate", type=float, default=3e-3)
 
-    parser.add_argument("--max_epochs", type=int, default=1500)
+    parser.add_argument("--max_epochs", type=int, default=1000)
     parser.add_argument("--gpus", type=int, default=-1)
     parser.add_argument("--tpu_cores", type=int, default=None)
     parser.add_argument("--cpu", action="store_true")
